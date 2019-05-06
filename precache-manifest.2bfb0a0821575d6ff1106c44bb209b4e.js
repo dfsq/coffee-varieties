4a298deb982d82d770eaab23c88c8df813f4e48d
dfsq <astashenkau@gmail.com>
2019-05-06 09:39:28 +0200

    Updates

---
+++ precache-manifest.2bfb0a0821575d6ff1106c44bb209b4e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2765ebc17ae4c85975b210ada0591bc0",
+    "url": "/coffee-varieties/index.html"
+  },
+  {
+    "revision": "5e1a5f38ec2f7f0a5b16",
+    "url": "/coffee-varieties/static/css/main.526eea72.chunk.css"
+  },
+  {
+    "revision": "60a81f26b95e76666a9e",
+    "url": "/coffee-varieties/static/js/2.93d3ad6e.chunk.js"
+  },
+  {
+    "revision": "5e1a5f38ec2f7f0a5b16",
+    "url": "/coffee-varieties/static/js/main.b95e9505.chunk.js"
+  },
+  {
+    "revision": "c7ab2512be3a2c7ad7dc",
+    "url": "/coffee-varieties/static/js/runtime~main.9db50c16.js"
+  },
+  {
+    "revision": "9fdd5b1ac96990e05e36959f5035e4aa",
+    "url": "/coffee-varieties/static/media/bag.9fdd5b1a.svg"
+  },
+  {
+    "revision": "eaab2af6a1e65969c94f5065e2944152",
+    "url": "/coffee-varieties/static/media/coffee-bean.eaab2af6.svg"
+  },
+  {
+    "revision": "95b7e24e3f030960e7a213693ec93aa3",
+    "url": "/coffee-varieties/static/media/cup.95b7e24e.svg"
+  },
+  {
+    "revision": "cc35250f83015a472fddae258bb12717",
+    "url": "/coffee-varieties/static/media/empty-coffee-cup.cc35250f.jpg"
+  }
+]);
